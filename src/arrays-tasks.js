da36e66764413b37dbac72b2fede228f93df02ee
Grigori Konopelko <grigori.konopelko@gmail.com>
2024-01-22 21:49:53 +0300

    task 1 and 2

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,12 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const arr = [];
+  for (let i = start; i <= end; i += 1) {
+    arr.push(i);
+  }
+  return arr;
 }
 
 /**
@@ -37,8 +41,21 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const result = [];
+  let length;
+
+  if (arr1.length >= arr2.length) {
+    length = arr1.length;
+  } else {
+    length = arr2.length;
+  }
+  for (let i = 0; i < length; i += 1) {
+    const a = arr1[i] === undefined ? 0 : arr1[i];
+    const b = arr2[i] === undefined ? 0 : arr2[i];
+    result.push(a + b);
+  }
+  return result;
 }
 
 /**
